bf8ec493b01269193671db3446b096846872a945
Fernando Medrano <fernando.medrano@upa.edu.mx>
2014-07-06 03:22:00 -0500

    show url

--- search.js
+++ search.js
@@ -57,6 +57,7 @@ var timer = setInterval(function(){
 				var urls = data.statuses[index]["entities"]["urls"];
 				//console.log(urls[urls.length - 1]["expanded_url"]);
 				//pool.insert({"url":urls[urls.length - 1]["expanded_url"]});
+				console.log(urls[urls.length - 1]["expanded_url"]);
 				q.push(urls[urls.length - 1]["expanded_url"], function (err) {console.log("Error on pooling: " + err);});
 				search_parameters["max_id"] = (search_parameters["max_id"] > data.statuses[index]["id"] || !search_parameters["max_id"]) ? data.statuses[index]["id"] : search_parameters["max_id"];
 				i++;
