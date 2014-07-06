2f52bb55b3f6a6d40573c412772e889830661a5c
Fernando Medrano <fernando.medrano@upa.edu.mx>
2014-07-06 03:06:00 -0500

    geting checkin id

--- search.js
+++ search.js
@@ -22,7 +22,17 @@ var q = async.queue(function (shortUrl, callback) {
     urlExpander.expand(shortUrl, function(err, longUrl){
         counter++;
         //console.log(counter+": "+longUrl);
-        expanded.insert({"longurl" : longUrl});
+        var fsq_params	= longUrl.split("/");
+        var fsq_keys	= fsq_params[fsq_params.length-1].split('?');
+        var fsq_checkinid = fsq_keys[0];
+        var fsq_signature = null;
+        if(fsq_keys.length > 1){
+        	var lastvars = fsq_keys[1].split('&');
+        	var almost_signature = lastvars[0].split('=');
+        	if(almost_signature[1].length > 3) :
+            	fsq_signature = almost_signature[1];
+    	}
+        expanded.insert({"longurl" : longUrl, "4sqr_checkinid" : fsq_checkinid, "fsq_signature": fsq_signature});
         callback();
     });
 }, 50);
@@ -45,7 +55,7 @@ var timer = setInterval(function(){
 			for (var index in data.statuses){
 				var urls = data.statuses[index]["entities"]["urls"];
 				//console.log(urls[urls.length - 1]["expanded_url"]);
-				pool.insert({"url":urls[urls.length - 1]["expanded_url"]});
+				//pool.insert({"url":urls[urls.length - 1]["expanded_url"]});
 				q.push(urls[urls.length - 1]["expanded_url"], function (errr) {console.log(errr);});
 				search_parameters["max_id"] = (search_parameters["max_id"] > data.statuses[index]["id"] || !search_parameters["max_id"]) ? data.statuses[index]["id"] : search_parameters["max_id"];
 				i++;
