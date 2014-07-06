92056b4e6411395a8d172a05cd0afe2bf18897ae
Fernando Medrano <fernando.medrano@upa.edu.mx>
2014-07-06 00:32:36 -0500

    mongo init

--- search.js
+++ search.js
@@ -1,4 +1,6 @@
 var twitterAPI = require('node-twitter-api');
+var MongoClient = require('mongodb').MongoClient;
+var format = require('util').format;
 var twitter = new twitterAPI({
     consumerKey: 'Alnn9DVS5HwuGlNrKwUAtw',
     consumerSecret: 'xcZsBZDwtubjDDIsfXYYB8Y1p2nJY9gk920a4C8wDws',
@@ -7,15 +9,25 @@ var twitter = new twitterAPI({
 
 var accessTokenKey = "182570549-oQ0DDxVQ0oNhKK4cvFYVSJ4FWcOXQf0oAIznt3CB";
 var accessTokenSecret="7giyVk47AAWbz5vue8ep1iC4uWEBeAqonsf8zXGwDU55x";
+var monk = require('monk');
+var db = monk('localhost:27017/todos_tweets');
+var pool = db.get("url");
 
-twitter.search({"q":"4sq com", "count":"5"},accessTokenKey,accessTokenSecret, function(error, data, response){
-	if (error){
-		console.log(error);
-	} else {
-		console.log(data);
-		for (var index in data.statuses){
-			var urls = data.statuses[index]["entities"]["urls"];
-			console.log(urls[urls.length - 1]["expanded_url"]);
+
+var max_id = null;
+	twitter.search({"q":"4sq com", "count":"5", "max_id":max_id},accessTokenKey,accessTokenSecret, function(error, data, response){
+		if (error){
+			console.log(error);
+		} else {
+			console.log(data);
+			for (var index in data.statuses){
+				var urls = data.statuses[index]["entities"]["urls"];
+				console.log(urls[urls.length - 1]["expanded_url"]);
+				pool.insert({"url":urls[urls.length - 1]["expanded_url"]});
+			}
+			max_id = data.search_metadata.max_id;
+			console.log("max_id: " + max_id);
 		}
-	}
-});
+	});
+
+	db.close();
